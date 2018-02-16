ffb827b97c76d2c9d07fe259de624363f9df9755
AkshayRaman97 <akshay.raman97@gmail.com>
2018-02-16 19:52:44 +0530

    Completed API integration

--- microservices/api/src/server.py
+++ microservices/api/src/server.py
@@ -1,13 +1,32 @@
 from src import app
-# from flask import jsonify
+from flask import jsonify,request
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
 
+nlu = NaturalLanguageUnderstandingV1(
+  username='50182f00-5d1d-4bec-9fe1-e211d621fe9e',
+  password='QXFaCLAJKplK',
+  version='2017-02-27')
 @app.route("/")
 def home():
     return "Hasura Hello World"
 
-# Uncomment to add a new URL at /new
-
-# @app.route("/json")
-# def json_message():
-#     return jsonify(message="Hello World")
+@app.route("/intent",methods=['POST'])
+def intent():
+    try:
+        text = request.json['text']
+        response = nlu.analyze(text=text,
+            features=Features(
+            entities=EntitiesOptions(
+            emotion=True,
+            sentiment=True,
+            limit=2),
+            keywords=KeywordsOptions(
+            emotion=True,
+            sentiment=True,
+            limit=2)))
+        return(json.dumps(response, indent=2))
+    except KeyError:
+        return(jsonify({"message":"Invalid request"}))
