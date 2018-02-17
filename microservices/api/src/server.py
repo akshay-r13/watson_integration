34458ab73c2b80297ed051a16ff78bd3ce08c0e8
AkshayRaman97 <akshay.raman97@gmail.com>
2018-02-17 20:10:37 +0530

    Completed API

--- microservices/api/src/server.py
+++ microservices/api/src/server.py
@@ -3,7 +3,7 @@ from flask import jsonify,request
 import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
-
+from watson_developer_cloud.watson_service import WatsonApiException
 
 nlu = NaturalLanguageUnderstandingV1(
   username='50182f00-5d1d-4bec-9fe1-e211d621fe9e',
@@ -30,3 +30,7 @@ def intent():
         return(json.dumps(response, indent=2))
     except KeyError:
         return(jsonify({"message":"Invalid request"}))
+    except WatsonApiException:
+        return(jsonify({"message":"Not enough text"}))
+    else:
+        return(jsonify({"message":"Some error occurred"}))
